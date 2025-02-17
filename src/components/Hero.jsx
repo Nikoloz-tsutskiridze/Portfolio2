a96b8a1649fd2a4f77df73e5e0ba64670cb4acb1
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2025-02-17 15:26:56 +0400

    Upgrade

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,21 @@ function Hero() {
   return (
     <section className="flex flex-col items-center justify-center">
       <div className="lg:flex md:items-center mt-8">
-        <h1 className="lg:mt-16 md:mt-8 mt-6 overflow-hidden text-[10vw] md:text-[6vw] lg:text-[10vw] font-semibold uppercase leading-none ml-10">
-          Nikoloz <br />
-          Tsutskiridze
-        </h1>
+        <div className="flex flex-col items-start">
+          <div className="lg:mt-8">
+            <h4 className="text-lg md:text-xl lg:text-2xl font-medium text-lime-300 lg:ml-12 ml-10 ">
+              Full-stack developer
+            </h4>
+            <h1 className="overflow-hidden text-[10vw] md:text-[6vw] lg:text-[10vw] font-semibold uppercase leading-none ml-10">
+              Nikoloz <br />
+              Tsutskiridze
+            </h1>
+          </div>
+        </div>
         <img
           src={img}
           alt="Nikoloz Tsutskiridze"
-          className="lg:mt-3 lg:h-[24rem] h-[20rem] md:h-[18rem] md:mt-4 sm:h-[22.2rem] sm:mt-4 object-cover"
+          className="lg:mt-3 lg:h-[24rem] h-[20rem] md:h-[18rem] sm:h-[22.2rem] object-cover"
         />
       </div>
       <div className="mb-6 mt-12">
@@ -21,7 +28,7 @@ function Hero() {
           target="_blank"
           rel="noopener noreferrer"
           download
-          className="flex items-center rounded-xl bg-lime-300 p-2 px-3 font-sans font-medium text-black hover:bg-lime-400"
+          className="flex items-center rounded-xl bg-lime-300 p-2 px-3 font-sans font-medium text-black hover:bg-lime-400 transition"
         >
           <span>Resume.pdf</span>
           <LuImport className="ml-2" />
